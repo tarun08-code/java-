2697f0a0a9ed2aac49edf415a31d90abea8e5179
dinesh <dineshk231105@gmail.com>
2025-08-05 22:13:24 +0530

    gn

--- While.java
+++ While.java
@@ -1,11 +1,19 @@
 class While {
     public static void main (String[] args) {
-      int n = 1;
-      while (n<=5) {
-
-        System.out.println(n);
-        n++;
-      } 
       
-    }
+       int x=8;
+      do {
+       System.out.println(x);
+       x--;
+      } while (x >= 1);
+
+     
+ 
+ for ( int i = 1; i<=3; i++){
+ 
+ for ( int j=5; j<=6; j++){
+ System.out.println (i +" "+ j);
+         }
+      }
+   }
 }
